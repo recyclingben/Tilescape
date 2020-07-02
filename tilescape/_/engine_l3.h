20ae291c8f5b86a5e6dc55aaaf93470f7926423b
recyclingben <bdcubbage@gmail.com>
2020-07-01 22:18:13 -0400

    Use branchless max operation.

--- tilescape/_/engine_l3.h
+++ tilescape/_/engine_l3.h
@@ -1,7 +1,11 @@
 #pragma once
 #include "../world_def.h"
+#include "math.h"
 #include "engine_l1.h"
 #include "engine_l2.h"
+
+#define JUMP_STRENGTH       53 /* 0-127 */
+#define JUMP_SHORT_STRENGTH 15 /* < JUMP_STRENGTH */
 
 
 void ts_engine_l3_step(ts_World *world)
@@ -10,12 +14,39 @@ void ts_engine_l3_step(ts_World *world)
 
     table_head(world->robots, &robot);
     while (robot) {
-        /* We could do this without if statements but I doubt it would be faster. */
-        if (robot->controller.push_n) ts_engine_l2_push_robot_y(world, robot, -4);
-        if (robot->controller.push_s) ts_engine_l2_push_robot_y(world, robot,  4);
-        if (robot->controller.push_e) ts_engine_l2_push_robot_x(world, robot,  4);
-        if (robot->controller.push_w) ts_engine_l2_push_robot_x(world, robot, -4);
+        ts_engine_l2_PushFlags flags_x = ts_engine_l2_push_robot_x(world, robot, robot->velocity_x);
+        ts_engine_l2_PushFlags flags_y = ts_engine_l2_push_robot_y(world, robot, robot->velocity_y);
+
+        if (flags_y & ts_engine_l2_PushFlags_FLOOR)
+            robot->velocity_y = 0;
+        if (flags_x & ts_engine_l2_PushFlags_LHIT
+            || flags_x & ts_engine_l2_PushFlags_RHIT)
+            robot->velocity_x = 0;
+
+        robot->velocity_y += 1;
+        if (robot->controller.push_n) robot->velocity_y -= 8;
+        if (robot->controller.push_s) robot->velocity_y += 8;
+        if (robot->controller.push_e) robot->velocity_x += 8;
+        if (robot->controller.push_w) robot->velocity_x -= 8;
+
+        robot->velocity_x -= robot->velocity_x / 4;
+        robot->velocity_y -= robot->velocity_y / 32;
+
+        if (abs(robot->velocity_x) < 4)
+            robot->velocity_x = 0;
+
+        if (robot->controller.jump)
+            robot->velocity_y = -JUMP_STRENGTH;
+        else if (robot->controller.jump_finish
+             &&  robot->velocity_y < 0)
+            /* Smaller jump. */
+            robot->velocity_y = umax8(robot->velocity_y, -JUMP_SHORT_STRENGTH);
+
+        robot->controller.jump        = false;
+        robot->controller.jump_finish = false;
 
         table_next(world->robots, robot, &robot);
     }
+
+    ++world->tick;
 }
---
+++ tilescape/_/math.h
@@ -0,0 +1,7 @@
+static inline int8_t max8(int8_t a, int8_t b) {
+    return a - ((a - b) & (a - b) >> 31);
+}
+
+static inline uint8_t umax8(uint8_t a, uint8_t b) {
+    return (uint8_t)max8((int8_t)a, (int8_t)b);
+}
