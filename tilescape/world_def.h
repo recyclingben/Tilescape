2fab0923fe34a87eae987a6f478d10a2642e830a
recyclingben <bdcubbage@gmail.com>
2020-07-03 12:21:56 -0400

    Add jump and velocity for some struct definitions.

--- tilescape/world_def.h
+++ tilescape/world_def.h
@@ -10,16 +10,18 @@ typedef struct {
 } ts_Tile;
 
 typedef struct {
-    ts_Tile tiles[(uint8_t)~0 + 1]
-                 [(uint8_t)~0 + 1];
+    ts_Tile tiles[(uint8_t)~0 + 1][(uint8_t)~0 + 1];
 } ts_Blob;
 
 typedef struct {
+    uint8_t tick;
     index_Index2D *blobs;
     table_Table *robots;
 } ts_World;
 
 typedef struct {
+    bool jump;
+    bool jump_finish;
     bool push_n;
     bool push_s;
     bool push_e;
@@ -27,6 +29,8 @@ typedef struct {
 } ts_Controller;
 
 typedef struct {
+    int8_t velocity_x;
+    int8_t velocity_y;
     ts_Place place_x;
     ts_Place place_y;
     ts_Controller controller;
