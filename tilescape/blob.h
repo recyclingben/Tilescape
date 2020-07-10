abd85fc079c881fead2c6f1db1418727984cbf37
recyclingben <bdcubbage@gmail.com>
2020-07-10 19:05:44 -0400

    Add functions for making and freeing blobs.

--- tilescape/blob.h
+++ tilescape/blob.h
@@ -2,6 +2,18 @@
 #include "world_def.h"
 
 
+static inline void ts_blob_make(ts_Blob *blob,
+                                ts_Blob **out_blob)
+{
+    *out_blob = malloc(sizeof(ts_Blob));
+    memcpy(*out_blob, blob, sizeof(ts_Blob));
+}
+
+static inline void ts_blob_free(ts_Blob *blob)
+{
+    free(blob);
+}
+
 static inline void ts_blob_get_tile(ts_Blob *blob,
                                     uint8_t x,
                                     uint8_t y,
