8d6388f6eccbe15fdd29927b702366c3c3f0ab2f
recyclingben <bdcubbage@gmail.com>
2020-09-07 15:09:58 -0400

    Remove function for getting robot controller; You should just use <robot>->controller now.

--- tilescape/robot.h
+++ tilescape/robot.h
@@ -17,9 +17,3 @@ static inline ts_Place ts_robot_get_place_y(ts_Robot *robot)
 {
     return robot->place_y;
 }
-
-static inline void ts_robot_get_controller(ts_Robot *robot,
-                                           ts_Controller **out_controller)
-{
-    *out_controller = &robot->controller;
-}
